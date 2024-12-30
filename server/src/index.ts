f91a7c3e9c2cc0fc15d4fc853ae238453cc7daf2
aurora-mei <huyencttde180614@fpt.edu.vn>
2024-12-30 21:47:49 +0700

    hiding key

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,7 +16,7 @@ app.post("/decks", async (req: Request, res: Response) => {
 });
 mongoose
   .connect(
-    "mongodb+srv://huyencttde180614:nQHMYsphFe04tgBb@cluster0.emwa2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://huyencttde180614:[key]@cluster0.emwa2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => {
     console.log(`Connected to database in ${PORT}`);
